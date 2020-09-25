36465a4ea265abc0c933724e7206cbf2b4d19ffa
SimonovaKatya <simonova.322.228.1488@gmail.com>
2020-09-25 16:52:43 +0700

    Добавьте файлы проекта.

---
+++ 17laboratory/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace _17laboratory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.textBox_Num = new System.Windows.Forms.TextBox();
+            this.textBox_lamda2 = new System.Windows.Forms.TextBox();
+            this.textBox_lamda1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(-1, 125);
+            this.chart1.Name = "chart1";
+            series3.BorderColor = System.Drawing.Color.Red;
+            series3.BorderWidth = 3;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Color = System.Drawing.Color.Red;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.BorderColor = System.Drawing.Color.Green;
+            series4.BorderWidth = 3;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Color = System.Drawing.Color.Green;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(975, 489);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(12, 25);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(140, 71);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "START";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // textBox_Num
+            // 
+            this.textBox_Num.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_Num.Location = new System.Drawing.Point(279, 12);
+            this.textBox_Num.Name = "textBox_Num";
+            this.textBox_Num.Size = new System.Drawing.Size(100, 29);
+            this.textBox_Num.TabIndex = 2;
+            this.textBox_Num.Text = "10";
+            // 
+            // textBox_lamda2
+            // 
+            this.textBox_lamda2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_lamda2.Location = new System.Drawing.Point(279, 82);
+            this.textBox_lamda2.Name = "textBox_lamda2";
+            this.textBox_lamda2.Size = new System.Drawing.Size(100, 29);
+            this.textBox_lamda2.TabIndex = 3;
+            this.textBox_lamda2.Text = "4";
+            // 
+            // textBox_lamda1
+            // 
+            this.textBox_lamda1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox_lamda1.Location = new System.Drawing.Point(279, 47);
+            this.textBox_lamda1.Name = "textBox_lamda1";
+            this.textBox_lamda1.Size = new System.Drawing.Size(100, 29);
+            this.textBox_lamda1.TabIndex = 4;
+            this.textBox_lamda1.Text = "1,5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(158, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number points";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(187, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Lamda1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(187, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Landa2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 626);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_lamda1);
+            this.Controls.Add(this.textBox_lamda2);
+            this.Controls.Add(this.textBox_Num);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.TextBox textBox_Num;
+        private System.Windows.Forms.TextBox textBox_lamda2;
+        private System.Windows.Forms.TextBox textBox_lamda1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 17laboratory/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17laboratory
+{
+    public partial class Form1 : Form
+    {
+
+        int n;
+        List<double> first = new List<double>();
+        List<double> third = new List<double>();
+
+        List<double> lenght1 = new List<double>();
+
+        List<double> lenght3 = new List<double>();
+
+        int i;
+        Random rnd = new Random();
+        double lamda1;
+        double lamda2;
+        double lamda3;
+        double price1 = 0;
+        double price2 = 0;
+        double price3 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+
+            lamda1 = Convert.ToDouble(textBox_lamda1.Text);
+            lamda2 = Convert.ToDouble(textBox_lamda2.Text);
+            lamda3 = lamda1 + lamda2;
+
+            price1 = 0;
+            price2 = 0;
+            price3 = 0;
+            i = 0;
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            n = Convert.ToInt32(textBox_Num.Text);
+            chart1.ChartAreas[0].AxisX.Maximum = n;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 1;
+
+
+            chart1.Series[0].Points.AddXY(price1, 0);
+            chart1.Series[1].Points.AddXY(price2, 0);
+
+            first.Add(price1);
+            third.Add(price3);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double r = rnd.NextDouble();
+            double a = -1 * (Math.Log(r) / lamda1);
+            double b = -1 * (Math.Log(r) / lamda2);
+            double c = -1 * (Math.Log(r) / lamda3);
+
+            price1 = price1 + a;
+            price2 = price2 + b;
+            price3 = price3 + c;
+
+            if (price1 > price2)
+            {
+                first.Add(price2);
+                first.Add(price1);
+
+            }
+            else
+            {
+                first.Add(price1);
+                first.Add(price2);
+
+            }
+
+            double eqq = third[third.Count - 1];
+            third.Add(price3);
+            lenght3.Add(price3 - eqq);
+
+            chart1.Series[0].Points.AddXY(price1, 0);
+            chart1.Series[1].Points.AddXY(price2, 0);
+            chart1.Series[1].Color = Color.Red;
+
+            i++;
+            if (i - 1 == n)
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
